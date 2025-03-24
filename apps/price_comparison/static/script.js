e4331c0f2ce57b09d5f1b9f7a4e3846b3cfc7963
Vansh Anand <anandvansh34@gmail.com>
2025-03-25 00:00:48 +0530

    feat: Added sort by price and rating feature using javascript

---
+++ apps/price_comparison/static/script.js
@@ -0,0 +1,70 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    function compareByPrice(prod1, prod2) {
+        return (parseFloat(prod1.price) < parseFloat(prod2.price) ? -1 : 1);
+    }
+
+    function compareByRating(prod1, prod2) {
+        return (parseFloat(prod1.rating) < parseFloat(prod2.rating) ? -1 : 1);
+    }
+
+    function sortByPrice() {
+        return data.sort(compareByPrice)
+    }
+
+    function sortByRating() {
+        return data.sort(compareByRating)
+    }
+
+    function updateTableContent(data) {
+        const t = document.getElementById("sortable-table");
+
+        for (let i = 1; i < t.rows.length; i++) {
+            t.rows[i].children[0].innerHTML = data[i - 1].name;
+            t.rows[i].children[1].innerHTML = data[i - 1].price;
+            t.rows[i].children[2].innerHTML = data[i - 1].rating;
+            t.rows[i].children[3].innerHTML = data[i - 1].platform;
+            t.rows[i].children[4].children[0].href = data[i - 1].product_url;
+        }
+    }
+
+    function isPriceSorted(prodArray) {
+
+        for (let i = 1; i < prodArray.length; i++) {
+            if (prodArray[i].price < prodArray[i - 1].price) return false;
+        }
+        return true;
+    }
+
+    function isRatingSorted(prodArray) {
+
+        for (let i = 1; i < prodArray.length; i++) {
+            if (prodArray[i].rating < prodArray[i - 1].rating) return false;
+        }
+        return true;
+    }
+
+    document.getElementById("sort-price").addEventListener('click', () => {
+
+        if (isPriceSorted(data)) {
+            updateTableContent(data.reverse());
+        } else {
+            data = sortByPrice();
+            updateTableContent(data);
+        }
+
+    });
+
+    document.getElementById("sort-rating").addEventListener('click', () => {
+
+        if (isRatingSorted(data)) {
+            updateTableContent(data.reverse());
+        } else {
+            data = sortByRating();
+            console.log(data);
+            updateTableContent(data);
+        }
+
+    });
+})       
