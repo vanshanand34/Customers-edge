1b6743d6a5d48dd7d16b0954dca4e5484c3ff0de
Vansh Anand <anandvansh34@gmail.com>
2026-08-16 23:03:02 +0530

    feat: UI refactoring changes, related hanlding in scripts

--- apps/price_comparison/static/parseSearchResults.js
+++ apps/price_comparison/static/parseSearchResults.js
@@ -1,6 +1,7 @@
 function truncateWords(text, truncateLength) {
-  const words = text.split(" ");
-  if (words.length <= truncateLength) return text;
+  if (!text) return "";
+  const words = text?.split(" ");
+  if (words?.length <= truncateLength) return text;
   let truncatedStr = "";
   for (let i = 0; i < truncateLength; i++) {
     truncatedStr += words[i] + " ";
@@ -8,8 +9,14 @@ function truncateWords(text, truncateLength) {
   return truncatedStr + "...";
 }
 
+function escapeHTML(value) {
+  const div = document.createElement("div");
+  div.textContent = value ?? "";
+  return div.innerHTML;
+}
+
 function parseSearchResults() {
-  let searchResultsCollections = [];
+  let searchResultsCollections = searchResults;
   const search_text = searchText;
   const searchTable = document.getElementById("search-table-body");
   const websocketUrl = "ws://" + window.location.host + "/ws/search-results/";
@@ -28,6 +35,12 @@ function parseSearchResults() {
       searchTable.appendChild(tableRow);
       return;
     }
+
+    if (data.type != "product") {
+      console.log("Not a product", data);
+      return;
+    }
+
     // console.log(data);
     data.price = parseFloatCustom(data.price);
     data.rating = parseFloatCustom(data.rating);
@@ -35,25 +48,56 @@ function parseSearchResults() {
     const tableRow = document.createElement("tr");
     const truncatedText = truncateWords(data.name, 8);
 
+    const productName = escapeHTML(data.name);
+    const platform = escapeHTML(data.platform);
+    const truncatedName = escapeHTML(truncatedText);
+
     tableRow.innerHTML = `
-        <td class="prod-name" data-value="${data.name}" title="${data.name}">
+        <td
+          class="prod-name"
+          data-value="${productName}"
+          title="${productName}"
+        >
           ${truncatedText}
         </td>
-        <td>${data.price}</td>
-        <td>${data.rating}</td>
-        <td>${data.platform}</td>
-        <td>
-        <a href="${data.product_url}">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 128 128">
-            <path d="M 84 11 C 82.3 11 81 12.3 81 14 C 81 15.7 82.3 17 84 17 L 106.80078 17 L 60.400391 63.400391 C 59.200391 64.600391 59.200391 66.499609 60.400391 67.599609 C 61.000391 68.199609 61.8 68.5 62.5 68.5 C 63.2 68.5 63.999609 68.199609 64.599609 67.599609 L 111 21.199219 L 111 44 C 111 45.7 112.3 47 114 47 C 115.7 47 117 45.7 117 44 L 117 14 C 117 12.3 115.7 11 114 11 L 84 11 z M 24 31 C 16.8 31 11 36.8 11 44 L 11 104 C 11 111.2 16.8 117 24 117 L 84 117 C 91.2 117 97 111.2 97 104 L 97 59 C 97 57.3 95.7 56 94 56 C 92.3 56 91 57.3 91 59 L 91 104 C 91 107.9 87.9 111 84 111 L 24 111 C 20.1 111 17 107.9 17 104 L 17 44 C 17 40.1 20.1 37 24 37 L 69 37 C 70.7 37 72 35.7 72 34 C 72 32.3 70.7 31 69 31 L 24 31 z"></path>
-          </svg>
-        </a>
-      </td>`;
+
+        <td class="prod-price">
+          ${data.price}
+        </td>
+
+        <td class="prod-rating">
+          <span class="rating-star">★</span>
+          <span class="rating-value">${data.rating}</span>
+        </td>
+
+        <td class="prod-platform">
+          <span class="platform-badge">
+            ${platform}
+          </span>
+        </td>
+
+        <td class="prod-link">
+          <a
+            href="${data.product_url}"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="View product"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 128 128"
+            >
+              <path d="M 84 11 C 82.3 11 81 12.3 81 14 C 81 15.7 82.3 17 84 17 L 106.80078 17 L 60.400391 63.400391 C 59.200391 64.600391 59.200391 66.499609 60.400391 67.599609 C 61.000391 68.199609 61.8 68.5 62.5 68.5 C 63.2 68.5 63.999609 68.199609 64.599609 67.599609 L 111 21.199219 L 111 44 C 111 45.7 112.3 47 114 47 C 115.7 47 117 45.7 117 44 L 117 14 C 117 12.3 115.7 11 114 11 L 84 11 z M 24 31 C 16.8 31 11 36.8 11 44 L 11 104 C 11 111.2 16.8 117 24 117 L 84 117 C 91.2 117 97 111.2 97 104 L 97 59 C 97 57.3 95.7 56 94 56 C 92.3 56 91 57.3 91 59 L 91 104 C 91 107.9 87.9 111 84 111 L 24 111 C 20.1 111 17 107.9 17 104 L 17 44 C 17 40.1 20.1 37 24 37 L 69 37 C 70.7 37 72 35.7 72 34 C 72 32.3 70.7 31 69 31 L 24 31 z"></path>
+            </svg>
+          </a>
+        </td>
+    `;
 
     searchTable.appendChild(tableRow);
     const rowIndex = searchTable.children.length - 1;
     const delay = rowIndex * 60;
     addAnimationClass(tableRow, delay);
+    searchResults = searchResultsCollections;
   };
 
   function addAnimationClass(tableRow, delay) {
